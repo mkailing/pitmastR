93c77602af5ac123d7e2ee4fa4c04acce3740431
mkailing <mkailing@vt.edu>
2025-03-30 19:31:58 -0400

    updates to functions

--- R/3explorRs.R
+++ R/3explorRs.R
@@ -20,7 +20,7 @@
 #' # obs.groups =  c(id, sex, site.x),
 #' # obs.unit = "weeks",
 #' # date.colname = date)
-inds_by_dates <- function(x, #name of working dataframe
+bundle_by <- function(x, #name of working dataframe
                               obs.groups = c(id), #list of columns to group by
                               obs.unit = c("weeks","months","years"), #date units that the user wants to summarize activity by
                               date.colname) { #name of date column in x for function to search by
@@ -45,14 +45,14 @@ inds_by_dates <- function(x, #name of working dataframe
   return(y)
 }
 
-## This used the test tag dataframe to create a survival object
+
 #' @title
-#' Creates survival object
+#' Create survival object for use in 'survival' package
 #' 
 #' @description
-#' Uses test tag fires to create 'sampling data frame' for survival analyses
+#' Uses several previously built dataframes to create 'sampling data frame' from individuals observations for survival analyses
 #' 
-#' @param x main data object, preferably returned from 'workhorse'
+#' @param map.file1 main data object, preferably returned from 'workhorse'
 #' @param obs.groups list of columns by which observations will be grouped
 #' @param obs.unit time unit of which to summarize observations
 #' @param date.colname name of the column containing dates
@@ -64,5 +64,58 @@ inds_by_dates <- function(x, #name of working dataframe
 #' # obs.groups =  c(id, sex, site.x),
 #' # obs.unit = "weeks",
 #' # date.colname = date)
-
+pit_survival <- function(m1, pit_workr, readr_check, min.reads = 2, min.detects = 0){
+  
+  #establish all possible sampling dates between reader start and end date
+  m1 %>%
+    #group_by(reader) %>%
+    transmute(reader, date = purrr::map2(start.date, end.date, seq, by = '1 day')) %>%
+    tidyr::unnest(cols=c(date)) -> all.dates
+  
+  #determine reader operation dates based on test tag fires
+  samp.days <- left_join(all.dates, readr_check, by = c('reader', 'date')) %>%
+    group_by(reader) %>%
+    mutate(tt.fires = ifelse(is.na(tt.fires), 0, tt.fires),
+           op = ifelse(is.na(op), "UNK", op)) %>%
+    filter(op == "YES"|tt.fires>min.reads)
+  
+  #create df of all possible dates an individual could be observed between capture date and study (ie last dates readers were on)
+  ind.dates <- pit_workr %>%
+    tidyr::drop_na(date.tagged) %>%
+    group_by(id) %>%
+    mutate(date.tagged = min(date.tagged)) %>%
+    ungroup() %>%
+    group_by(id, start.date, reader) %>%
+    filter(row_number()==1) %>%
+    ungroup() %>%
+    group_by(id) %>%
+    transmute(id, reader, date = purrr::map2(date.tagged, end.date, seq, by = '1 day')) %>%
+    tidyr::unnest(cols = c(date, reader))
+  
+  #calculate the number of detections an individual was detected in a night to code event: FALSE = detected/censored, TRUE = not detected/dead
+  pit_sumr <- pit_workr %>%
+    tidyr::drop_na(date.tagged) %>%
+    group_by(id) %>%
+    mutate(date.tagged = min(date.tagged)) %>%
+    ungroup() %>%
+    group_by(id, date, reader, date.tagged) %>%
+    summarise(tot.dets = length(unique(time)))
+  
+  #last, remove the dates readers were down (ie test tag fires below minimum acceptable defined by user) because there was no observation/follow-up occurring
+  pit_obsr <- left_join(ind.dates, pit_sumr, by = c('id', 'date', 'reader')) %>%
+    group_by(id) %>%
+    tidyr::fill(c(reader, date.tagged), .direction = 'downup') %>%
+    ungroup() %>%
+    mutate(tot.dets = ifelse(is.na(tot.dets), 0, tot.dets)) %>%
+    semi_join(samp.days, by = c('date','reader')) %>%
+    group_by(id) %>%
+    arrange(date) %>%
+    mutate(time_since_lastobs = ifelse(date == min(date), 
+                                       as.numeric(difftime(date, date.tagged, units = 'days')),
+                                       as.numeric(difftime(date, lag(date), units = 'days'))),
+           time_since_tagdate = as.numeric(difftime(date, date.tagged, units = 'days')),
+           event = ifelse(tot.dets>=min.detects, FALSE, TRUE))
+  
+  return(pit_obsr)
+}
 
--- R/4system_checkRs.R
+++ R/4system_checkRs.R
@@ -20,8 +20,8 @@ readr_check <- function(x, sys.colname, date1, min.reads = 2){
   z <- x %>%
     group_by({{sys.colname}}, {{date1}}) %>% #should have the option to use date or converted dates
     summarise(tt.fires = length(unique(time))) %>%
-    mutate(op = ifelse(tt.fires>=min.reads, TRUE, FALSE)) %>%
-    rename(system = 1, date = 2)
+    mutate(op = ifelse(tt.fires>=min.reads, "YES", "NO")) %>%
+    rename(reader = 1, date = 2)
   return(z)
 }
 
@@ -30,11 +30,9 @@ readr_check <- function(x, sys.colname, date1, min.reads = 2){
 #' Visualizes system operation
 #' 
 #' @description
-#' Plots the number of times a test tag was fired, indicating whether reader was on based on object returned by 'readr_check'.
+#' Plots the number of times a test tag was fired, indicating whether reader was operating. 
 #' 
 #' @param df dataframe with all test tag reads
-#' @param site.colname name of the column where unique system names are stored (the grouping variable by which to summarize tag reads)
-#' @param date.colname name of the column where date variable (the time grouping variable by which to summarize tag reads)
 #' @export
 #' @details 
 #' this seems optional!
@@ -42,7 +40,7 @@ readr_check <- function(x, sys.colname, date1, min.reads = 2){
 #' # TBD
 readr_viz <- function(df) {
   p <- ggplot(data = subset(df)) +
-    facet_wrap(~system, scales = 'free') +
+    facet_wrap(~reader, scales = 'free') +
     geom_col(aes(x=date, y = tt.fires, fill = op)) +
     scale_fill_manual(name = "Reader on", values = c("red","blue")) +
     ylab("Count of test tag fires") +
